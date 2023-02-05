0460e1fd35d9c57b3488f9c3425182b554c6637c
KarolZygadlo <karol.zygadlo@protonmail.com>
2023-02-05 18:53:43 +0100

    added basic layout elements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter
 import customtkinter
-from pytube import YouTube
+from pytube import YouTube 
 
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
@@ -8,5 +8,19 @@ customtkinter.set_default_color_theme("blue")
 app = customtkinter.CTk()
 app.geometry("720x480")
 app.title("YouTube Videos Downloader")
+
+url_var = tkinter.StringVar()
+title = customtkinter.CTkLabel(app, text="Insert a youtube link", textvariable=url_var)
+title.pack(padx=10, pady=10)
+
+link = customtkinter.CTkEntry(app, width=350, height=40)
+link.pack()
+
+download = customtkinter.CTkButton(app, text="Download", command=startDownload)
+download.pack()
+
+
+
+
 
 app.mainloop()
