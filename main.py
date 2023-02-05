588772c0849fc70400186c880c022b6cfbafe171
KarolZygadlo <karol.zygadlo@protonmail.com>
2023-02-05 18:35:41 +0100

    update, initial commit, created app window

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter
+import customtkinter
+from pytube import YouTube
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("YouTube Videos Downloader")
+
+app.mainloop()
